7e39873dc48f02ef4f6cf15d5ec6c265104a494f
macarro <migmackle@alum.us.es>
2020-05-04 10:15:47 +0200

    Fileloc with text

--- ImputeApp.py
+++ ImputeApp.py
@@ -18,23 +18,46 @@ class ImputeApp:
 
         self.data = None
 
-        self.data_msg = tk.StringVar(self.root)
-        self.data_msg.set("No file loaded.")
+        #self.data_msg = tk.StringVar(self.root)
+        #self.data_msg.set("No file loaded.")
 
         self.data_info = tk.StringVar(self.root)
         self.data_info.set("")
+
+        self.indata_selection_frame_fileloc_text = None
 
         self.make_widgets()
 
         self.root.mainloop()
 
     def make_widgets(self):
+        # Input data selection frame
+        # --------------------------
+        indata_selection_frame = tk.Frame(
+            self.root, bg='red', height=50, width=230)
+        indata_selection_frame.grid(row=1, column=1, padx=10, pady=10)
+        indata_selection_frame.grid_propagate(0)
 
-        data_frame = tk.Frame(self.root, bg='red', height=50, width=300)
-        data_frame.grid(row=1, column=1, padx=10, pady=10)
+        indata_selection_frame_l1 = tk.Label(
+            indata_selection_frame, text='File:')
+        indata_selection_frame_l1.grid(row=1, column=1, sticky='W')
 
-        data_label = tk.Label(data_frame, textvariable=self.data_msg)
-        data_label.grid(row=1, column=1, padx=5, pady=50)
+        self.indata_selection_frame_fileloc_text = tk.Text(
+            indata_selection_frame, width=20, height=1, state='disabled')
+        self.indata_selection_frame_fileloc_text.grid(
+            row=2, column=1, sticky='W')
+        self.indata_selection_frame_fileloc_text.configure(state='normal')
+        self.indata_selection_frame_fileloc_text.insert(1.0, 'No file loaded.')
+        self.indata_selection_frame_fileloc_text.configure(state='disabled')
+
+
+        #data_label = tk.Label(
+        #    indata_selection_frame, textvariable=self.data_msg)
+        #data_label.grid(row=2, column=1, padx=5, pady=5)
+
+        indata_characteristics_frame = tk.Frame(
+            self.root, bg='green', height=50, width=230)
+        indata_characteristics_frame.grid(row=1, column=2, padx=10, pady=10)
 
         info_frame = tk.Frame(self.root, bg='blue', height=50, width=300)
         info_frame.grid(row=2, column=1, padx=10, pady=10)
@@ -59,10 +82,21 @@ class ImputeApp:
             initialdir="/", title="Select file",
             filetypes=(("csv files", "*.csv"), ("all files", "*.*")))
         self.data = pd.read_table(filename)
-        self.data_msg.set('File: ' + filename)
+        #self.data_msg.set('File: ' + filename)
+
+        self.indata_selection_frame_fileloc_text.configure(state='normal')
+        self.indata_selection_frame_fileloc_text.delete('1.0', tk.END)
+        self.indata_selection_frame_fileloc_text.insert(1.0, filename)
+        self.indata_selection_frame_fileloc_text.tag_add(
+            "all", 1.0, tk.END)
+        self.indata_selection_frame_fileloc_text.tag_config(
+            "all", background="yellow", foreground="blue", wrap=tk.CHAR)
+
+        #self.indata_selection_frame_fileloc_text.configure(state='disabled')
+
         self.data_info.set(str(self.data))
 
 
 if __name__ == "__main__":
-    app = ImputeApp()
+    imputeApp = ImputeApp()
 
